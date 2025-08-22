120359648ebecb26d79d722ac0a6d5833f235896
ak <kothari.atit@gmail.com>
2025-08-22 14:16:30 -0700

    add clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,18 @@ export default function RootLayout({
           data-domain="mindmap.theperseuslabs.com"
           src="https://pl-plausible.194.195.92.250.sslip.io/js/script.file-downloads.hash.outbound-links.pageview-props.tagged-events.js"
         />
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "sz02lhumkp");
+          `}
+        </Script>
       </head>
       <body className={inter.className}>
         <ThemeProvider
